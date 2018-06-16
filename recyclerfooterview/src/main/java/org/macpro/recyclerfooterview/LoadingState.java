ebcd00875b12f6c3f34776367c248c3e710c0cb3
Pikachu <55527267@qq.com>
2018-06-16 09:06:27 +0800

    新增枚举类型

---
+++ recyclerfooterview/src/main/java/org/macpro/recyclerfooterview/LoadingState.java
@@ -0,0 +1,17 @@
+package org.macpro.recyclerfooterview;
+
+/**
+ * 作者： macpro  on 2018/6/16.
+ * 邮箱： xxx.com
+ */
+public enum LoadingState {
+
+    // 加载中、加载完成、加载到底、加载出错了
+    LOADING(1), LOAD_COMPLETE(2), LOAD_END(3), LOAD_ERROR(4);
+
+    private int mState;
+
+    LoadingState(int state) {
+        this.mState = state;
+    }
+}
